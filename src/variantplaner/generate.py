a7c45fc4a964ebce20528012b6a4eeaa1920891c
Pierre Marijon <pierre.marijon-ext@aphp.fr>
2023-05-09 13:49:21 +0200

    fix(parquet): set tmpdir in variantplaner merge run

--- src/variantplaner/generate.py
+++ src/variantplaner/generate.py
@@ -3,10 +3,14 @@
 # std import
 from __future__ import annotations
 
+import logging
+
 # 3rd party import
 import polars
 
 # project import
+
+logger = logging.getLogger("generate")
 
 
 def transmission_ped(
@@ -48,27 +52,56 @@ def transmission(
 
     Returns:
          DataFrame with transmission information.
+         With gt, ad, dp and gq for index, mother and father.
+         If any of them isn't present value are set to polars.Null (0 for gt)
+         Columns transmission contains: index_gt * 100 + mother_gt * 10 + father_gt
     """
     group_lf = genotypes_lf.groupby("id").all().collect()
     group_lf = group_lf.filter(polars.col("sample").arr.contains(index_name))
 
+    logger.debug(f"{group_lf.row(0)}")
+
     # I assume sample order is all time the same but I'm not sure
     sample2index = {sample: idx for idx, sample in enumerate(group_lf.row(0)[1], start=0)}
+
+    logger.debug(f"{sample2index}")
 
     transmission_lf = group_lf.with_columns(
         polars.col("gt").arr.get(sample2index[index_name]).alias("index_gt"),
         polars.col("ad").arr.get(sample2index[index_name]).alias("index_ad"),
         polars.col("dp").arr.get(sample2index[index_name]).alias("index_dp"),
         polars.col("gq").arr.get(sample2index[index_name]).alias("index_gq"),
-        polars.col("gt").arr.get(sample2index[mother_name]).alias("mother_gt"),
-        polars.col("ad").arr.get(sample2index[mother_name]).alias("mother_ad"),
-        polars.col("dp").arr.get(sample2index[mother_name]).alias("mother_dp"),
-        polars.col("gq").arr.get(sample2index[mother_name]).alias("mother_gq"),
-        polars.col("gt").arr.get(sample2index[father_name]).alias("father_gt"),
-        polars.col("ad").arr.get(sample2index[father_name]).alias("father_ad"),
-        polars.col("dp").arr.get(sample2index[father_name]).alias("father_dp"),
-        polars.col("gq").arr.get(sample2index[father_name]).alias("father_gq"),
     )
+
+    if mother_name in sample2index:
+        transmission_lf = transmission_lf.with_columns(
+            polars.col("gt").arr.get(sample2index[mother_name]).alias("mother_gt"),
+            polars.col("ad").arr.get(sample2index[mother_name]).alias("mother_ad"),
+            polars.col("dp").arr.get(sample2index[mother_name]).alias("mother_dp"),
+            polars.col("gq").arr.get(sample2index[mother_name]).alias("mother_gq"),
+        )
+    else:
+        transmission_lf = transmission_lf.with_columns(
+            polars.lit(0).alias("mother_gt"),
+            polars.lit(None).alias("mother_ad"),
+            polars.lit(None).alias("mother_dp"),
+            polars.lit(None).alias("mother_gq"),
+        )
+
+    if father_name in sample2index:
+        transmission_lf = transmission_lf.with_columns(
+            polars.col("gt").arr.get(sample2index[father_name]).alias("father_gt"),
+            polars.col("ad").arr.get(sample2index[father_name]).alias("father_ad"),
+            polars.col("dp").arr.get(sample2index[father_name]).alias("father_dp"),
+            polars.col("gq").arr.get(sample2index[father_name]).alias("father_gq"),
+        )
+    else:
+        transmission_lf = transmission_lf.with_columns(
+            polars.lit(0).alias("father_gt"),
+            polars.lit(None).alias("father_ad"),
+            polars.lit(None).alias("father_dp"),
+            polars.lit(None).alias("father_gq"),
+        )
 
     transmission_lf = transmission_lf.with_columns(
         (polars.col("index_gt") * 100 + polars.col("mother_gt") * 10 + polars.col("father_gt")).alias("origin"),
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -29,3 +29,41 @@ def test_transmission(tmp_path: pathlib.Path) -> None:
     truth = DATA_DIR / "transmission.parquet"
 
     filecmp.cmp(truth, out_path)
+
+
+def test_transmission_missing_mother(tmp_path: pathlib.Path) -> None:
+    """Check add_origin of genotype."""
+    out_path = tmp_path / "transmission.parquet"
+
+    genotypes_lf = polars.scan_parquet(DATA_DIR / "no_info.genotypes.parquet")
+    genotypes_lf = genotypes_lf.filter(polars.col("sample") != "sample_3")
+
+    pedigree_lf = polars.scan_parquet(DATA_DIR / "sample.parquet")
+
+    transmission = generate.transmission_ped(genotypes_lf, pedigree_lf)
+
+    polars.Config.set_tbl_cols(500)
+
+    assert transmission.get_column("mother_gt").to_list() == [0, 0, 0, 0, 0]
+    assert transmission.get_column("mother_ad").to_list() == [None, None, None, None, None]
+    assert transmission.get_column("mother_dp").to_list() == [None, None, None, None, None]
+    assert transmission.get_column("mother_gq").to_list() == [None, None, None, None, None]
+
+
+def test_transmission_missing_father(tmp_path: pathlib.Path) -> None:
+    """Check add_origin of genotype."""
+    out_path = tmp_path / "transmission.parquet"
+
+    genotypes_lf = polars.scan_parquet(DATA_DIR / "no_info.genotypes.parquet")
+    genotypes_lf = genotypes_lf.filter(polars.col("sample") != "sample_2")
+
+    pedigree_lf = polars.scan_parquet(DATA_DIR / "sample.parquet")
+
+    transmission = generate.transmission_ped(genotypes_lf, pedigree_lf)
+
+    polars.Config.set_tbl_cols(500)
+
+    assert transmission.get_column("father_gt").to_list() == [0, 0, 0, 0, 0]
+    assert transmission.get_column("father_ad").to_list() == [None, None, None, None, None]
+    assert transmission.get_column("father_dp").to_list() == [None, None, None, None, None]
+    assert transmission.get_column("father_gq").to_list() == [None, None, None, None, None]
